d80f71689b0f71bfcf784866ef25a596e17b7bf0
woohyun212 <woohyun212@gmail.com>
2024-09-09 01:11:36 +0900

    🗑️ Remove deprecated code for Django PlayerStats model refactoring
    - Deleted old code related to PlayerStats model in Django
    - Preparing for new model structure

--- nadia/serializers.py
+++ nadia/serializers.py
@@ -8,30 +8,3 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ['url', 'username', 'email', 'is_staff']
-
-
-class CharacterStatSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CharacterStatModel
-        fields = [
-        'player_stats', 'average_rank', 'character_code', 'max_killings', 'top3', 'top3_rate', 'total_games', 'usages',
-        'wins']
-
-
-class PlayerStatsSerializer(serializers.HyperlinkedModelSerializer):
-    character_stats = CharacterStatSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = PlayerStatsModel
-        fields = '__all__'
-
-        # TODO : extra_kwargs 관련 내용 정리 하기
-        # 난 pk 가 아닌 nickname 으로 데이터를 불러오려고 했었음
-        # pk가 아닌 다른 값을 통해 detail 조회를 하려고하니 아래와 같은 오류가 났었음
-        # Could not resolve URL for hyperlinked relationship using view name "playerstatsmodel-detail".
-        # You may have failed to include the related model in your API, or incorrectly configured the `lookup_field` attribute on this field.
-        # extra_kwargs 속성 추가 후 정상화
-
-        extra_kwargs = {
-            'url': {'lookup_field': 'nickname'}
-        }
--- nadia/urls.py
+++ nadia/urls.py
@@ -1,18 +1,15 @@
 from django.urls import path, include
 from nadia.views import UserViewSet
 from rest_framework import routers
-from .views import PlayerStatsViewSet
 
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
-router.register(r'playerstats', PlayerStatsViewSet, basename='playerstatsmodel')
 
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
-    # path('playerstats/<str:nickname>/', PlayerStatsViewSet.as_view({'get': 'retrieve'})),
 ]
