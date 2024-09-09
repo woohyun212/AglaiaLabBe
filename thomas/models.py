b2b70fd816cd7356da4683dcec4b617b0f9ad831
woohyun212 <woohyun212@gmail.com>
2024-09-09 18:31:50 +0900

    🎉 feat: Create GameInfo, BattleRecord, EquipmentAndTraits models
    - GameInfo, BattleRecord, EquipmentAndTraits 모델 생성
    - BattleRecord, EquipmentAndTraits는 GameInfo에 외래키로 연결됨

--- thomas/models.py
+++ thomas/models.py
@@ -9,7 +9,7 @@ class GameInfo(models.Model):
     season_id = models.IntegerField()  # 시즌 ID
     matching_mode = models.IntegerField()  # 2: 일반, 3: 랭크
     matching_team_mode = models.IntegerField()  # 1: 솔로, 2: 듀오, 3: 스쿼드
-    server_name = models.CharField()  # 배틀 서버 지역 이름
+    server_name = models.CharField(max_length=100)  # 배틀 서버 지역 이름
     character_num = models.IntegerField()  # 캐릭터 번호
     start_dtm = models.DateTimeField()  # 게임 시작 시간
     play_time = models.IntegerField()  # 유저 플레이 시간 (초)
@@ -23,14 +23,25 @@ class GameInfo(models.Model):
     pre_made = models.IntegerField()  # 사전 구성된 팀원 수
     victory = models.BooleanField()  # 승리 여부
     game_rank = models.IntegerField()  # 최종 등수
+    mmr_before = models.IntegerField(null=True)  # 이전 mmr
+    mmr_gain = models.IntegerField(null=True)  # mmr 변화량
+    mmr_after = models.IntegerField(null=True)  # 변경된 mmr
 
     def __str__(self):
         return f"Game {self.game_id} - Player {self.nickname}"
+
+    class Meta:
+        unique_together = ('user_num', 'game_id')
+        indexes = [
+            models.Index(fields=['user_num']),
+            models.Index(fields=['game_id']),
+        ]
 
 
 # 전투 기록 테이블
 class BattleRecord(models.Model):
     game = models.ForeignKey(GameInfo, on_delete=models.CASCADE)  # 게임 정보와 연결
+    user_num = models.BigIntegerField()  # GameInfo의 user_num을 저장
     character_level = models.IntegerField()  # 캐릭터 레벨
     player_kill = models.IntegerField()  # 킬 수
     player_assistant = models.IntegerField()  # 어시스트 수
@@ -57,21 +68,47 @@ class BattleRecord(models.Model):
     total_triple_kill = models.IntegerField()  # 연속 3킬
     total_quadra_kill = models.IntegerField()  # 연속 4킬
     total_extra_kill = models.IntegerField()  # 연속 5킬 이상
+
     killer_user_num = models.IntegerField()  # 처형자의 유저 번호
-    killer = models.CharField(max_length=100)  # 처형자 구분
-    kill_detail = models.CharField(max_length=100)  # 처형자 닉네임
-    killer_character = models.CharField(max_length=100)  # 처형자 캐릭터
-    killer_weapon = models.CharField(max_length=100)  # 처형자 무기
-    cause_of_death = models.CharField(max_length=100)  # 사망 원인
-    place_of_death = models.IntegerField()  # 사망 지역
+    killer = models.CharField(max_length=100, null=True)  # 처형자 구분
+    kill_detail = models.CharField(max_length=100, null=True)  # 처형자 닉네임
+    killer_character = models.CharField(max_length=100, null=True)  # 처형자 캐릭터
+    killer_weapon = models.CharField(max_length=100, null=True)  # 처형자 무기
+    cause_of_death = models.CharField(max_length=100, null=True)  # 사망 원인
+    place_of_death = models.IntegerField(null=True)  # 사망 지역
+
+    killer_user_num2 = models.IntegerField()  # 처형자의 유저 번호2
+    killer2 = models.CharField(max_length=100, null=True)  # 처형자 구분2
+    kill_detail2 = models.CharField(max_length=100, null=True)  # 처형자 닉네임2
+    killer_character2 = models.CharField(max_length=100, null=True)  # 처형자 캐릭터2
+    killer_weapon2 = models.CharField(max_length=100, null=True)  # 처형자 무기2
+    cause_of_death2 = models.CharField(max_length=100, null=True)  # 사망 원인2
+    place_of_death2 = models.IntegerField(null=True)  # 사망 지역2
+
+    killer_user_num3 = models.IntegerField()  # 처형자의 유저 번호3
+    killer3 = models.CharField(max_length=100, null=True)  # 처형자 구분3
+    kill_detail3 = models.CharField(max_length=100, null=True)  # 처형자 닉네임3
+    killer_character3 = models.CharField(max_length=100, null=True)  # 처형자 캐릭터3
+    killer_weapon3 = models.CharField(max_length=100, null=True)  # 처형자 무기3
+    cause_of_death3 = models.CharField(max_length=100, null=True)  # 사망 원인3
+    place_of_death3 = models.IntegerField(null=True)  # 사망 지역3
+
+    def save(self, *args, **kwargs):
+        # game 필드의 GameInfo 객체에서 user_num 값을 자동으로 가져옴
+        self.user_num = self.game.user_num
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"Battle record for game {self.game.game_id}"
+
+    class Meta:
+        unique_together = ('game', 'user_num')
 
 
 # 장비 및 특성 정보 테이블
 class EquipmentAndTraits(models.Model):
     game = models.ForeignKey(GameInfo, on_delete=models.CASCADE)  # 게임 정보와 연결
+    user_num = models.BigIntegerField()  # GameInfo의 user_num을 저장
     skin_code = models.IntegerField()  # 사용한 스킨
     best_weapon = models.IntegerField()  # 가장 높은 무기 숙련도 번호
     best_weapon_level = models.IntegerField()  # 가장 높은 무기 숙련도 레벨
@@ -85,5 +122,13 @@ class EquipmentAndTraits(models.Model):
     tactical_skill_group = models.IntegerField()  # 전술 스킬 그룹
     tactical_skill_level = models.IntegerField()  # 전술 스킬 레벨
 
+    def save(self, *args, **kwargs):
+        # game 필드의 GameInfo 객체에서 user_num 값을 자동으로 가져옴
+        self.user_num = self.game.user_num
+        super().save(*args, **kwargs)
+
     def __str__(self):
         return f"Equipment and Traits for game {self.game.game_id}"
+
+    class Meta:
+        unique_together = ('game', 'user_num')
