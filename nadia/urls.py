4cbb33410e369e6434be1371c3de98752cc9578a
woohyun212 <woohyun212@gmail.com>
2024-09-11 04:37:53 +0900

    ✨ Add viewsets for CharacterStats, MMRHistory, Player, and PlayerStats models and FBV - Add Urls for created viewsets  and FBV.

--- nadia/urls.py
+++ nadia/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import UserViewSet
+from .views import UserViewSet, PlayerViewSet, PlayerStatsViewSet, MMRHistoryViewSet, CharacterStatsViewSet
+from .views import register_player
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
+router.register(r'players', PlayerViewSet)
+router.register(r'player-stats', PlayerStatsViewSet)
+router.register(r'mmr-history', MMRHistoryViewSet)
+router.register(r'character-stats', CharacterStatsViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+
+    path('register/<str:nickname>/', register_player, name='register_player'),
 ]
--- nadia/views.py
+++ nadia/views.py
@@ -1,27 +1,107 @@
-from django.contrib.admin.utils import lookup_field
-from django.shortcuts import render
-
-# Create your views here.
-
-# user view set  ###########
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from .serializers import *
 
+from bser_api import BserAPI
+from AglaiaLabBE.settings.base import ER_API_KEY
 
-# ViewSets define the view behavior.
+# BserAPI Instance
+ER = BserAPI(ER_API_KEY)
+CURRENT_SEASON = 27
+
+
+# user view set  ###########
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
 ######################################
-from rest_framework import viewsets, status
 from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from .serializers import *
+from thomas.models import BattleRecord
+from django.db.models import Sum, Avg
+from pprint import pp
 
-import requests
-from bser_api import BserAPI
-from AglaiaLabBE.settings.base import ER_API_KEY
 
-# BserAPI Instance
-er_api = BserAPI(ER_API_KEY)
-CURRENT_SEASON = 25
+# Player ViewSet
+class PlayerViewSet(viewsets.ModelViewSet):
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
+
+
+# PlayerStats ViewSet
+class PlayerStatsViewSet(viewsets.ModelViewSet):
+    queryset = PlayerStats.objects.all()
+    serializer_class = PlayerStatsSerializer
+
+
+# MMRHistory ViewSet
+class MMRHistoryViewSet(viewsets.ModelViewSet):
+    queryset = MMRHistory.objects.all()
+    serializer_class = MMRHistorySerializer
+
+
+# CharacterStats ViewSet
+class CharacterStatsViewSet(viewsets.ModelViewSet):
+    queryset = CharacterStats.objects.all()
+    serializer_class = CharacterStatsSerializer
+
+
+########################################
+# 전적 검색 Page 필요한 데이터 구성 FBV
+########################################
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def register_player(request, nickname):
+    if not nickname:
+        return Response({"error": "Nickname parameter is required."}, status=status.HTTP_404_NOT_FOUND)
+    user_num = ER.get_user_num(nickname=nickname)
+    try:
+        player, is_created = Player.objects.get_or_create(nickname=nickname, user_num=user_num)
+        player_serializer = PlayerSerializer(player)
+
+        player_stats, is_created = update_or_create_player_stats(player)
+        player_stats_serializer = PlayerStatsSerializer(player_stats)
+        return Response(player_stats_serializer.data, status=status.HTTP_201_CREATED)
+    except Exception as e:
+        print(e)
+        return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+
+def update_or_create_player_stats(player: Player, season_id: int = CURRENT_SEASON):
+    er_user_stats = ER.fetch_user_stats(user_num=player.user_num, season_id=season_id)
+    for er_user_stat in er_user_stats:
+        # 전적 데이터 검색
+        return PlayerStats.objects.update_or_create(
+            player=player,
+            season_id=season_id,
+            matching_mode=er_user_stat.get('matchingMode'),
+            matching_team_mode=er_user_stat.get('matchingTeamMode'),
+            defaults={
+                'mmr': er_user_stat.get('mmr'),
+                'rank': er_user_stat.get('rank'),
+                'rank_size': er_user_stat.get('rankSize'),
+                'rank_percent': er_user_stat.get('rankPercent'),
+                'total_games': er_user_stat.get('totalGames'),
+                'total_wins': er_user_stat.get('totalWins'),
+                'average_rank': er_user_stat.get('averageRank'),
+                'total_team_kills': er_user_stat.get('totalTeamKills'),
+                'average_kills': er_user_stat.get('averageKills'),
+                'average_assistants': er_user_stat.get('averageAssistants'),
+                'average_damage': round(BattleRecord.objects.filter(game__user_num__exact=player.user_num,
+                                                                    game__season_id__exact=season_id
+                                                                    ).aggregate(Avg('damage_to_player')
+                                                                                ).get('damage_to_player__avg')),
+                'average_hunts': er_user_stat.get('averageHunts'),
+                'top1': er_user_stat.get('top1'),
+                'top2': er_user_stat.get('top2'),
+                'top3': er_user_stat.get('top3'),
+                'top5': er_user_stat.get('top5'),
+                'top7': er_user_stat.get('top7'),
+            }
+        )
+
+    pass
